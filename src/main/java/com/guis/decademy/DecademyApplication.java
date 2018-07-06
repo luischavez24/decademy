c4e9a1f56a74af324fe8f0f741e677c01d28dd09
Raul0612 <caceresraul06@gmail.com>
2018-07-06 09:14:27 -0500

    Modificación de detalle de curso, index progreso

--- src/main/java/com/guis/decademy/DecademyApplication.java
+++ src/main/java/com/guis/decademy/DecademyApplication.java
@@ -39,18 +39,18 @@ public class DecademyApplication {
 	public List<Curso> cursos() {
 		List<Curso> cursos = new ArrayList<>();
 		cursos.add(new Curso("algo3", "Algoritmica 3","Desarrollar algoritmos con la finalidad de fortalecer los conocimientos adquiridos, poniendo énfasis en la eficiencia algorítmica, y las habilidades de elaboración de soluciones algorítmicas a problemas basados en un conjunto de componentes que deben interactuar entre si.", "algoritmica.png")
-				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg"))
-				.addProfesores(new Profesor("santiago.moquillaza", "Santiago", "Moquillaza", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/MoquillazaSantiago.jpg"))
-				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg")));
+				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("santiago.moquillaza", "Santiago", "Moquillaza", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/MoquillazaSantiago.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg",TipoUsuario.TIPO_TUTOR)));
 		
 		cursos.add(new Curso("ia", "Sistemas Operativos","En este curso aprenderemos los conceptos básicos asociados a la Inteligencia Artificial, las escuelas predominantes en esta área. Así como distintos algoritmos para la generación de la inteligencia en el computador", "sistemas_operativos.png")
-				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg"))
-				.addProfesores(new Profesor("ricardo.gareca", "Ricardo", "Gareca", "/img/ricardo.jpg"))
-				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg")));
+				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("ricardo.gareca", "Ricardo", "Gareca", "/img/ricardo.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg",TipoUsuario.TIPO_TUTOR)));
 		cursos.add(new Curso("so", "Inteligencia Artificial", "Conocerás como se estructura un Sistema Operativo, la forma como trabaja internamente el sistema operativo la gestión de los procesos. Daremos un vistazo a los SO predominantes en el mercado actualmente", "inteligencia_artificial.png")
-				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg"))
-				.addProfesores(new Profesor("jorge.diaz", "Jorge", "Diaz Muñante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Diaz_Jorge.jpg"))
-				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg")));
+				.addProfesores(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("jorge.diaz", "Jorge", "Diaz Muñante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Diaz_Jorge.jpg",TipoUsuario.TIPO_TUTOR))
+				.addProfesores(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Jorge_Chavez.jpg",TipoUsuario.TIPO_TUTOR)));
 				
 		return cursos;
 	}
@@ -58,11 +58,11 @@ public class DecademyApplication {
 	@Bean("profesores")
 	public List<Profesor> profesores() {
 		List<Profesor> profesores = new ArrayList<>();
-		profesores.add(new Profesor("jorge.diaz", "Jorge", "Diaz Muñante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Diaz_Jorge.jpg"));
-		profesores.add(new Profesor("santiago.moquillaza", "Santiago", "Moquillaza", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/MoquillazaSantiago.jpg"));
-		profesores.add(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg"));
-		profesores.add(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Chavez_Jorge.jpg"));
-		profesores.add(new Profesor("ricardo.gareca", "Ricardo", "Gareca", "ricardo.jpg"));
+		profesores.add(new Profesor("jorge.diaz", "Jorge", "Diaz Muñante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Diaz_Jorge.jpg","T"));
+		profesores.add(new Profesor("santiago.moquillaza", "Santiago", "Moquillaza", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/MoquillazaSantiago.jpg","T"));
+		profesores.add(new Profesor("victor.bustamante", "Victor", "Bustamante", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Bustamante_Victor.jpg","T"));
+		profesores.add(new Profesor("jorge.chavez", "Jorge", "Chavez", "http://sistemas.unmsm.edu.pe/fisi/adjuntos/paginas/Chavez_Jorge.jpg","T"));
+		profesores.add(new Profesor("ricardo.gareca", "Ricardo", "Gareca", "ricardo.jpg","T"));
 		return profesores;
 	}
 }
